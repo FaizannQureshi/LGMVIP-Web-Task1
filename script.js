1ab392790fc95db9f2fcf3dd1521c8ee7d9762eb
Muhammad Faizan Qureshi <99756478+FaizannQureshi@users.noreply.github.com>
2023-08-03 14:52:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") return;
+
+    const listItem = document.createElement("li");
+    listItem.textContent = taskText;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.onclick = function () {
+        taskList.removeChild(listItem);
+    };
+
+    listItem.appendChild(deleteButton);
+    taskList.appendChild(listItem);
+
+    taskInput.value = "";
+}
